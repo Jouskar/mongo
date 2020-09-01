629e0d1a84aa9b009afcc4dccfd9a22c3b701f25
Tolgahan Özbek <61375991+Jouskar@users.noreply.github.com>
2020-09-01 13:40:14 +0300

    user db

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.use(bodyParser.json());
 
 const mongoConnect = require('./utilities/database').mongoConnect;
 const Post = require('./models/post');
+const User = require('./models/user');
 
+//Post Functions
 app.post('/posts', (req, res)=> {
     const id = req.body.id;
     const title = req.body.title;
@@ -36,6 +38,32 @@ app.delete('/posts', (req, res)=>{
         .catch(err=>{console.log(err)});
 });
 
+//User Functions
+app.post('/users', (req, res)=> {
+    const id = req.body.id;
+    const name = req.body.name;
+    
+    const user = new User(id, name);
+    user.save();
+    res.send();
+});
+
+app.get('/users', (req, res)=>{
+    User.findAll()
+        .then(posts => {
+            res.send(posts);
+        });    
+});
+
+app.delete('/users', (req, res)=>{
+    const id = req.body.id;
+
+    User.deleteById(id)
+        .then(() => {
+            res.send("success");
+        })
+        .catch(err=>{console.log(err)});
+});
 mongoConnect(() => {
     app.listen(3000);
 });
---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { mongoConnect } = require('../utilities/database');
+
+const getDb = require('../utilities/database').getdb;
+
+class User{
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    save() {
+        const db = getDb();
+
+        db.collection('users')
+            .insertOne(this)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findAll() {
+        const db = getDb();
+        return db.collection('users')
+                .find({})
+                .toArray()
+                .then(posts => {
+                    return posts;
+                })
+                .catch(err => console.log(err));
+    }
+
+    static deleteById(userName) {
+        const db = getDb();
+        return db.collection('users')
+                .deleteOne({id: userName})
+                .then(()=>{console.log('deleted')})
+                .catch(err=> {console.log(err)});
+    }
+}
+
+module.exports = User;
